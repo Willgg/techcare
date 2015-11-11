67baea6c2e098fe52d1b958c8520de69b4c4bcad
ousmane sakho <sakho.ousmane@gmail.com>
2015-11-11 18:59:44 +0100

    adding trainees to adviser

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,17 @@
 class MessagesController < ApplicationController
 
   def create
+    @message = Message.new(params_messages)
   end
 
   def destroy
   end
+
+
+  private
+
+  def params_messages
+    params.require(:message).permit(:content, :sender_id, :recipient_id)
+  end
+
 end
