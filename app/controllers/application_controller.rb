4e09453f6f55f3d19ad3ca3db79386a6acfad222
Rémi BallotFlurin <remi.ballot@gmail.com>
2015-11-11 13:32:17 +0100

    sign up step 1 ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,5 +26,10 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :picture
+    devise_parameter_sanitizer.for(:sign_up) << :first_name
+    devise_parameter_sanitizer.for(:sign_up) << :last_name
+    devise_parameter_sanitizer.for(:sign_up) << :birthday
+    devise_parameter_sanitizer.for(:sign_up) << :sexe
+
   end
 end
