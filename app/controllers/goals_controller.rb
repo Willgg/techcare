90f08b7f9db631202fbee3d9c18e06624ab70db0
ousmane sakho <sakho.ousmane@gmail.com>
2015-11-12 15:52:42 +0100

    user to user features

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,6 +3,7 @@ class GoalsController < ApplicationController
 
   def index
     @goals = Goal.where(user: @user)
+    @message = Message.new
   end
 
   def new
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,17 +1,33 @@
 class MessagesController < ApplicationController
+  before_action :find_user, only: [:create]
 
   def create
-    @message = Message.new(params_messages)
+    @message = Message.new(messages_params)
+    @message.sender = current_user
+    if @user == current_user
+      @message.recipient = current_user.adviser
+    else
+      @message.recipient = @user
+    end
+    @message.save
+    redirect_to user_goals_path(@user)
+
   end
 
   def destroy
+    @message = Message.find(params[:id])
+    @message.destroy
   end
 
 
   private
 
-  def params_messages
-    params.require(:message).permit(:content, :sender_id, :recipient_id)
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  def messages_params
+    params.require(:message).permit(:content, :user)
   end
 
 end
