473c49c863376d529d79d20698dc7371bd0e9b53
William Godard <grenier.godard@gmail.com>
2015-11-16 00:01:35 +0100

    Front improvement

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ActiveRecord::Base
   validates :birthday, presence: true
 
   has_attached_file :picture,
-    styles: { medium: "300x300>", thumb: "100x100>" }
+    styles: { medium: "300x300#", thumb: "100x100#" }
 
   validates_attachment_content_type :picture,
     content_type: /\Aimage\/.*\z/
