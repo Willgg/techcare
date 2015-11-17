6ee9fd8aa408aa9136c3a269e710f25b579fb25c
William Godard <grenier.godard@gmail.com>
2015-11-17 15:23:04 +0100

    Get measures from withings

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,92 +1,72 @@
 class ProvidersController < ApplicationController
   require 'withings-api'
   require 'withings'
+
   include Withings
   include Withings::Api
-
 
   def index
   end
 
   def create
-    callback_url = "http://localhost:3000" + providers_callback_path
+    callback_url   = "http://localhost:3000" + providers_callback_path
     consumer_token = ConsumerToken.new(
                       ENV['WITHINGS_OAUTH_CONSUMER_KEY'],
                       ENV['WITHINGS_OAUTH_CONSUMER_SECRET']
                     )
     request_token_response = Withings::Api.create_request_token(consumer_token, callback_url)
-    request_token = request_token_response.request_token
+    request_token          = request_token_response.request_token
+
     current_user.api_consumer_key = request_token.key
     current_user.api_consumer_secret = request_token.secret
     current_user.save
-    # redirection du user vers la page d'autorisation Withings
-    redirect_to request_token_response.authorization_url
 
+    # Redirection to withings page for techcare autorization
+    redirect_to request_token_response.authorization_url
   end
 
   def save_token
-    # Reconstruire la signature
-    id = params[:userid]
-    oauth_consumer_key = ENV['WITHINGS_OAUTH_CONSUMER_KEY']
-    oauth_nonce = Random.rand(100000).to_s
-    oauth_signature_method = 'HMAC-SHA1'
-    oauth_token = current_user.api_consumer_key
-    oauth_timestamp = Time.now.to_i.to_s
-    oauth_version = '1.0'
 
-    url =  'https://oauth.withings.com/account/access_token?'
+    # Settings request_token and consumer_token
+    callback_url    = "http://localhost:3000" + providers_callback_path
+    request_token   = Withings::Api::RequestToken.new(current_user.api_consumer_key , current_user.api_consumer_secret)
+    consumer_key    = ENV['WITHINGS_OAUTH_CONSUMER_KEY']
+    consumer_secret = ENV['WITHINGS_OAUTH_CONSUMER_SECRET']
+    consumer_token  = ConsumerToken.new( consumer_key, consumer_secret )
 
-    parameters = 'oauth_consumer_key=' +
-                  oauth_consumer_key +
-                  '&oauth_nonce=' +
-                  oauth_nonce +
-                  '&oauth_signature_method=' +
-                  oauth_signature_method +
-                  '&oauth_timestamp=' +
-                  oauth_timestamp +
-                  '&oauth_token=' +
-                  oauth_token +
-                  '&oauth_version=' +
-                  oauth_version +
-                  '&userid='+
-                  id
+    # Get access token
+    access_token_response = Withings::Api.create_access_token(request_token, consumer_token, "user_id_not_currently_required_by_withings")
+    access_token          = access_token_response.access_token
+    user_id               = access_token_response.user_id
+    oauth_token           = access_token.key
+    oauth_token_secret    = access_token.secret
 
-    base_string = 'GET&' + CGI.escape(url) + CGI.escape(parameters)
+    # Persisting access token
+    current_user.api_consumer_key = oauth_token
+    current_user.api_consumer_secret = oauth_token_secret
+    current_user.api_user_id = params[:userid]
+    current_user.save
 
-    ## Cryptographic hash function used to generate oauth_signature
-    # by passing the secret key and base string. Note that & has
-    # been appended to the secret key. Don't forget this!
-    #
-    # This line of code is from a SO topic
-    # (http://stackoverflow.com/questions/4084979/ruby-way-to-generate-a-hmac-sha1-signature-for-oauth)
-    # with minor modifications.
-    secret_key = ENV['WITHINGS_OAUTH_CONSUMER_SECRET']
-    oauth_signature = CGI.escape(Base64.encode64("#{OpenSSL::HMAC.digest('sha1',secret_key, base_string)}").chomp)
+    # User data fetching
+    Withings.consumer_key    = consumer_key
+    Withings.consumer_secret = consumer_secret
 
-    # Construire la requete http
-
-    url = "https://oauth.withings.com/account/access_token?oauth_consumer_key=" + oauth_consumer_key + "&oauth_nonce=" + oauth_nonce + "&oauth_signature=" + oauth_signature + "&oauth_signature_method=" + oauth_signature_method + "&oauth_timestamp=" + oauth_timestamp + "&oauth_token=" + oauth_token + "&oauth_version=" + oauth_version + "&userid=" + id
-    url.slice!("%3D")
-    response = open(url).read
+    response  = Withings::Connection.get_request('/user', oauth_token, oauth_token_secret, :action => :getbyuserid, :userid => user_id)
+    user_data = response['users'].detect { |item| item['id'] == user_id.to_i }
+    user      = Withings::User.new(user_data.merge({:oauth_token => oauth_token, :oauth_token_secret => oauth_token_secret}))
 
     raise
-    current_user.user_id
-    access_token_response = Withings::Api.create_access_token(Withings::Api.create_request_token(consumer_token, callback_url).request_token, consumer_token, "user_id_not_currently_required_by_withings")
+    # Get data from scale device
+    user.measurement_groups(device: Withings::SCALE)
+    #user.measurement_groups(measurement_type: 1)
 
-    # access_token          = access_token_response.access_token
-
-    # user_id            = access_token_response.user_id
-    # oauth_token        = access_token.key
-    # oauth_token_secret = access_token.secret
-
-    @user = current_user
-    @user.api_user_id = params[:userid]
-    @user.api_consumer_key = params[:oauth_token]
-    @user.api_consumer_secret = params[:oauth_verifier]
-    @user.save
-    # api_consumer_key
-    # api_consumer_secret
-    # api_user_id
-    redirect_to user_goals_path(current_user)
+    # Redirection to dashboard
+    if current_user.is_adviser || !current_user.measures.empty?
+        flash[:notice] = "Your withings data has been synchronized"
+        redirect_to user_goals_path(current_user)
+    else
+        flash[:alert] = "Unable to synchronize your data"
+        redirect_to providers_path
+    end
   end
 end
