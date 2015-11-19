713f357f8ce4e11a5c3100268e11ad3d3e6ef3e8
Rémi BallotFlurin <remi.ballot@gmail.com>
2015-11-19 12:25:46 +0100

    added data fetch at sign in and description to advisers

--- app/models/adviser.rb
+++ app/models/adviser.rb
@@ -2,11 +2,12 @@
 #
 # Table name: advisers
 #
-#  id         :integer          not null, primary key
-#  title      :string
-#  user_id    :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id          :integer          not null, primary key
+#  title       :string
+#  user_id     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  description :text
 #
 # Indexes
 #
---
+++ app/services/trainees/fetch_measures_service.rb
@@ -0,0 +1,65 @@
+module Trainees
+ class FetchMeasuresService
+   def initialize(user)
+    Withings.consumer_key    = ENV['WITHINGS_OAUTH_CONSUMER_KEY']
+    Withings.consumer_secret = ENV['WITHINGS_OAUTH_CONSUMER_SECRET']
+    oauth_token = user.api_consumer_key
+    oauth_token_secret = user.api_consumer_secret
+    user_id = user.api_user_id
+
+    response  = Withings::Connection.get_request('/user', oauth_token, oauth_token_secret, :action => :getbyuserid, :userid => user_id)
+    user_data = response['users'].detect { |item| item['id'] == user_id.to_i }
+    @withings_user      = Withings::User.new(user_data.merge({:oauth_token => oauth_token, :oauth_token_secret => oauth_token_secret}))
+    @user = user
+
+   end
+
+   def fetch!
+
+    if @user.measures.exists?
+      last_measure_withings = @user.measures.where(source: "withings").order(created_at: :asc).last
+      data = @withings_user.measurement_groups(start_at: last_measure_withings.created_at, end_at: Time.current)
+
+    else
+      data = @withings_user.measurement_groups()
+    end
+    #user.measurement_groups(measurement_type: 1)
+    #user.measurement_groups(device: Withings::SCALE)
+    # Measures Creation
+    #if @user.measures.order(date: :asc).last
+
+    data.each do |measure|
+      if measure.weight
+        m = Measure.new
+        m.measure_type_id  = 1
+        m.value            = measure.weight
+        m.date             = measure.taken_at
+        m.user             = @user
+        m.source           = "withings"
+        m.save
+      end
+
+      if measure.systolic_blood_pressure
+        m = Measure.new
+        m.measure_type_id  = 2
+        m.value            = measure.systolic_blood_pressure
+        m.date             = measure.taken_at
+        m.user             = @user
+        m.source           = "withings"
+        m.save
+      end
+
+      if measure.ratio
+        m = Measure.new
+        m.measure_type_id  = 3
+        m.value            = measure.ratio
+        m.date             = measure.taken_at
+        m.user             = @user
+        m.source           = "withings"
+        m.save
+      end
+    end
+
+   end
+ end
+end
