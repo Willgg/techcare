ff7ed7cf6633b2149a07454dcd3e875fa0ccd8fa
William Godard <grenier.godard@gmail.com>
2015-12-03 16:51:26 +0100

    Done with delete in both coach and trainee view

--- app/assets/javascripts/app/menu.js
+++ app/assets/javascripts/app/menu.js
@@ -1,3 +1,10 @@
-$( '.messages').hover( function() {
-    $( '.menu' ).toggle();
+if ( $(window).width() > 1024) {
+  var jsEvent = 'mouseenter mouseleave'
+}
+else {
+  var jsEvent = 'click'
+}
+
+$( '.chat-card' ).on( jsEvent, '.message', function() {
+    $( '.menu', this ).toggle();
 });
