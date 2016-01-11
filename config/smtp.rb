1137e2cbccc237ba88bdf7b74afaac0c02daef48
William Godard <grenier.godard@gmail.com>
2016-01-11 23:27:27 +0100

    Clean SMTP conf file

--- config/smtp.rb
+++ config/smtp.rb
@@ -1,8 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :port =>           '587',
-  :address =>        'smtp.mandrillapp.com',
   :user_name =>      ENV['MANDRILL_USERNAME'],
   :password =>       ENV['MANDRILL_APIKEY'],
+  :address =>        'smtp.mandrillapp.com',
+  :port =>           587,
   :domain =>         'techcare.io',
-  :authentication => 'login'
+  :authentication => 'plain',
+  :enable_starttls_auto => true
 }
