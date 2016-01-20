ce0d01740db6c106e24f5936acd27547c3d85837
William Godard <grenier.godard@gmail.com>
2016-01-20 20:46:47 +0100

    Done with access_token

--- app/controllers/authorizations_controller.rb
+++ app/controllers/authorizations_controller.rb
@@ -6,57 +6,47 @@ class AuthorizationsController < ApplicationController
   skip_after_action :verify_authorized
 
   def create
-    @callback_url   = ENV['HOST'] + "/auth/#{@provider}/callback"
-    @consumer       = OAuth::Consumer.new(
+    callback_url   = ENV['HOST'] + "/auth/#{@provider}/callback"
+    consumer       = OAuth::Consumer.new(
                       ENV['FITBIT_CONSUMER_KEY'],
                       ENV['FITBIT_CONSUMER_SECRET'],
                       :site => "https://api.fitbit.com")
-    @request_token  = @consumer.get_request_token(:oauth_callback => @callback_url)
+    request_token  = consumer.get_request_token
+    session[:request_token] = { token: request_token.token, secret: request_token.secret}
+    session[:request_token_params] = request_token.params
 
-    # Create an new Authorization
-    @authorization = Authorization.new(
-                      token:  @request_token.token,
-                      secret: @request_token.secret,
-                      user:   current_user,
-                      source: @provider
-                    )
-    session[:request_token_params] = @request_token.params
-
-    if @authorization.save
-      redirect_to @request_token.authorize_url(oauth_callback: @callback_url)
-    else
-      flash[:alert] = I18n.t('controllers.providers.error', default: "Unable to synchronize your data.")
-      redirect_to providers_path
-    end
+    redirect_to request_token.authorize_url
   end
 
   def save_token
 
-    # Get back token and secret
-    @token          = params[:oauth_token]
-    @authorization  = Authorization.where(token: @token, user: current_user).last
-    @secret         = @authorization.secret
-
     # Rebuild the Consumer
-    @consumer       = OAuth::Consumer.new(ENV['FITBIT_CONSUMER_KEY'],ENV['FITBIT_CONSUMER_SECRET'], :site => "https://fitbit.com")
+    consumer       = OAuth::Consumer.new(
+                      ENV['FITBIT_CONSUMER_KEY'],
+                      ENV['FITBIT_CONSUMER_SECRET'],
+                      :site => "https://api.fitbit.com")
 
-    # Rebuild the RequestToken
-    @request_token = OAuth::RequestToken.new(@consumer, @token, @secret)
+    request_token  = OAuth::RequestToken.from_hash(consumer, {oauth_token: session[:request_token]["token"], oauth_token_secret: session[:request_token]["secret"]})
 
-    # Rebuild clean RequestToken params
-    @request_token_params = session[:request_token_params]
-    @request_token_params.each do |key,value|
-      @request_token_params[key.to_sym] = value
+    access_token   = request_token.get_access_token(oauth_verifier: params[:oauth_verifier])
+    uid            = access_token.params.select { |k,v| k.to_s.match(/user|id|uid/) }
+
+    @authorization = Authorization.new(
+                      user: current_user,
+                      token: access_token.token,
+                      secret: access_token.secret,
+                      source: params[:provider],
+                      uid: uid.values.last
+                    )
+    raise
+    if @authorization.save
+      flash[:notice] = I18n.t('controllers.providers.success', default: "Your data has been synchronized.")
+      redirect_to user_goals_path(current_user)
+    else
+      flash[:alert] = I18n.t('controllers.providers.error', default: "Unable to synchronize your data.")
+      redirect_to providers_path
     end
-    raise
 
-    # Add clean rebuilt params to RequestToken
-    @request_token.params = session[:request_token_params]
-
-    # Add oauth_verifier in #get_access_token
-    verifier = params[:oauth_verifier]
-    @access_token = @request_token.get_access_token(:oauth_verifier => verifier)
-    raise
   end
 
   def destroy
@@ -67,5 +57,6 @@ class AuthorizationsController < ApplicationController
   def find_provider
     @provider = params[:provider]
   end
+
 
 end
