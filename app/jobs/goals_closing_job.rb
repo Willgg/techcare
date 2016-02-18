7e3687ce4536785814b93810793d2fa4ba68e6e1
William Godard <grenier.godard@gmail.com>
2016-02-18 14:49:50 +0100

    Redesign of goal closing job

--- app/jobs/goals_closing_job.rb
+++ app/jobs/goals_closing_job.rb
@@ -3,28 +3,31 @@ class GoalsClosingJob < ActiveJob::Base
   queue_as :default
 
   def perform(*args)
-    puts "*** Performing GoalsClosingJob#perform ***"
-    # Get all the goals that are outdated and with no end_value
-    goals = Goal.where("end_date < ?", Time.current)
+    puts "*** Save the end_value of each outdated goal ***"
+
+    goals_over = Goal.where("end_date < ?", Time.current)
 
     unless goals.empty?
       goals.each do |goal|
-        puts "*** Processing Goal [id => #{goal.id}, type => #{goal.measure_type_id}]***"
-        # Save the end_value
+        puts "Processing Goal [id => #{goal.id}, type => #{goal.measure_type_id}]"
         if goal.end_value.nil?
           goal.end_value = goal.cumulative ? goal.sum_of_measures : goal.last_measure_for_user
-          puts "saved end_value = #{goal.end_value}" if goal.save
+          puts "For Goal#{goal.id}, end_value(#{goal.end_value}) has been saved" if goal.save
         end
+    end
 
-        # Create a new goal if not already succeed
-        unless goal.is_succeed?
-          puts "Goal is failed => CreateGoalService called"
-          trainee = goal.user
-          measure_type_id = goal.measure_type_id
-          Trainees::CreateGoalsService.new(trainee).only_for(measure_type_id)
+    puts "*** Check if new goals have to be created ***"
+
+    User.all.each do |user|
+      puts "Analyzing user : #{user.id}"
+      MeasureType.all.each do |mt|
+        puts "Check goals about #{mt.name}"
+        goals = user.goals.where(measure_type_id: mt.id)
+        unless goals.any? { |g| g.is_running? || g.is_succeed? }
+          Trainees::CreateGoalsService.new(user).only_for(mt.id)
         end
       end
     end
-    puts "*** GoalsClosingJob Performed ***"
+
   end
 end
