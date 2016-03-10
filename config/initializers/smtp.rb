c4c6186ebaffc9b05501ab1cea4cf567567b447c
William Godard <grenier.godard@gmail.com>
2016-03-10 16:16:00 +0100

    Emailing config

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,11 +1,9 @@
-Rails.application.configure do
-  config.action_mailer.smtp_settings = {
-    :user_name =>       ENV['MANDRILL_USERNAME'],
-    :password =>        ENV['MANDRILL_API_KEY'],
-    :address =>         'smtp.mandrillapp.com',
-    :port =>            587,
-    :domain =>          'techcare.io',
-    :authentication =>  'plain',
-    :enable_starttls_auto => true
-  }
-end
+ActionMailer::Base.smtp_settings = {
+  :user_name =>       ENV['POSTMARK_API_TOKEN'],
+  :password =>        ENV['POSTMARK_API_TOKEN'],
+  :address =>         'smtp.postmarkapp.com',
+  :port =>            '25',
+  :domain =>          'heroku.com',
+  :authentication =>  :cram_md5,
+  :enable_starttls_auto => true
+}
--- config/smtp.rb
+++
@@ -1,9 +0,0 @@
-ActionMailer::Base.smtp_settings = {
-  :user_name =>      ENV['MANDRILL_USERNAME'],
-  :password =>       ENV['MANDRILL_API_KEY'],
-  :address =>        'smtp.mandrillapp.com',
-  :port =>           587,
-  :domain =>         'techcare.io',
-  :authentication => 'plain',
-  :enable_starttls_auto => true
-}
