8232b8cb11b8ff698e5b56822d6eacca86c33113
William Godard <grenier.godard@gmail.com>
2016-03-11 18:43:06 +0100

    switch to postmark

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'techcare-production.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
   config.action_mailer.asset_host = { host: ENV.fetch("HOST") }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :user_name =>      ENV['MANDRILL_USERNAME'],
-  :password =>       ENV['MANDRILL_API_KEY'],
-  :address =>        'smtp.mandrillapp.com',
+  :user_name =>      ENV['POSTMARK_API_TOKEN'],
+  :password =>       ENV['POSTMARK_API_TOKEN'],
+  :address =>        'smtp.postmarkapp.com',
   :port =>           587,
   :domain =>         'techcare.io',
-  :authentication => 'plain',
+  :authentication => :cram_dm5,
   :enable_starttls_auto => true
 }
