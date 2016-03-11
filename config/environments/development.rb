b8de6aa391f4d458ae3acbede334e52ded527973
William Godard <grenier.godard@gmail.com>
2016-03-11 17:57:43 +0100

    fix config production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,7 +16,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
+  config.action_mailer.default_url_options = { host: 'techcare-production.herokuapp.com' }
   config.action_mailer.asset_host = "http://www.techcare.io"
 
   # Print deprecation notices to the Rails logger.
