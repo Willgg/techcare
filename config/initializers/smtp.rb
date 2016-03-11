35fc63c589c310e2528eaea54cc00574c5c8d879
William Godard <grenier.godard@gmail.com>
2016-03-11 19:11:21 +0100

    Fix config Postmark smtp

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :user_name =>      ENV['POSTMARK_API_TOKEN'],
   :password =>       ENV['POSTMARK_API_TOKEN'],
   :address =>        'smtp.postmarkapp.com',
-  :port =>           '25',
-  :domain =>         'heroku.com',
-  :authentication => :cram_dm5,
+  :port =>           587,
+  :domain =>         'techcare.io',
+  :authentication => 'plain',
   :enable_starttls_auto => true
 }
