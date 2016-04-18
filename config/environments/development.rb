23a067f22746fc54c60c1ca2d99fea3c4c6ec115
William Godard <grenier.godard@gmail.com>
2016-04-18 18:04:32 +0200

    Add letter_opener for dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,8 +15,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.default_url_options = { host: ENV['HOST'] || "localhost:3000" }
   config.action_mailer.asset_host = "http://www.techcare.io"
 
   # Print deprecation notices to the Rails logger.
