8cba13d8d01e2d2a02bf5f2f698e0b077889ea3b
William Godard <grenier.godard@gmail.com>
2016-06-23 19:41:11 +0200

    Fix bug on email url

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,13 +8,13 @@ class UserMailer < ApplicationMailer
   def welcome(user)
     @user = user
     @locale = (I18n.locale == I18n.default_locale ? nil : I18n.locale)
-    mail(to: @user.email, subject: I18n.t('mailers.user.title', name: @user.first_name))
+    mail to: @user.email, subject: I18n.t('mailers.user.welcome.title', name: @user.first_name)
   end
 
   def new_coach(user, coach)
     @user = user
     @coach = coach
     @locale = (I18n.locale == I18n.default_locale ? nil : I18n.locale)
-    mail to: @coach.email, subject: I18n.t('.new_user_subject', user: @user.first_name)
+    mail to: @coach.email, subject: I18n.t('mailers.user.trainee.title', trainee: @user.first_name)
   end
 end
