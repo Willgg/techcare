06dbfc19053df1a776ae42d9f261f921bc3f27d1
William Godard <grenier.godard@gmail.com>
2016-10-25 18:27:50 +0200

    Add new helper to round

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,12 @@ module UsersHelper
       [t('activerecord.attributes.user.sexe_option.' + s.downcase), s]
     end
   end
+
+  def round_weight(float)
+    if (float % 1) == 0
+      float.round(0)
+    else
+      float
+    end
+  end
 end
